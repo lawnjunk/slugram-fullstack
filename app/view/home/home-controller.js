6beb8b048f36bb721b013183c1b8030f5bf860e8
Duncan Marsh <dwolfm@gmail.com>
2016-11-09 10:38:57 -0800

    added all the things

---
+++ app/view/home/home-controller.js
@@ -0,0 +1,43 @@
+'use strict';
+
+require('./_home.scss');
+
+module.exports = ['$log', '$rootScope', '$window', '$location', 'authService',  HomeController ];
+
+function HomeController($log, $rootScope, $window, $location, authService){
+  $log.debug('init homeCtrl');
+
+  //let query = $location.search();
+  //console.log('query', query.token);
+  //this.lulawt = 'asldkfjlaskfjasf';
+
+  $window.onload = function(){
+    authService.getToken()
+    .then((token) => {
+      console.log('token', token)
+      $location.url('/home');
+    })
+    .catch(() => {
+      let query = $location.search();
+      if (query.token){
+        console.log('got token', query.token);
+        authService.setToken(query.token)
+        .then(() => {
+          $location.url('/home');
+        })
+      }
+    })
+  };
+
+  $rootScope.$on('locationChangeSuccess', $window.onload);
+
+  let googleAuthBase = 'https://accounts.google.com/o/oauth2/v2/auth';
+  let googleAuthResponseType = 'response_type=code';
+  let googleAuthClientID = `client_id=${__GOOGLE_CLIENT_ID__}`;
+  let googleAuthScope = 'scope=profile%20email%20openid';
+  let googleAuthRedirectURI = `redirect_uri=${__API_URL__}/api/auth/oauth_callback`;
+  let googleAuthAccessType = 'access_type=offline';
+  let googleAuthPrompt = 'prompt=consent';
+
+  this.googleAuthURL = `${googleAuthBase}?${googleAuthResponseType}&${googleAuthClientID}&${googleAuthScope}&${googleAuthRedirectURI}&${googleAuthAccessType}&${googleAuthPrompt}`;
+}
